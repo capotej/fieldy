9f30ef48a51cb296a5b378bda845b718b3244004
Darren Cauthon <darren@cauthon.com>
2015-04-14 14:57:17 -0500

    Flatten out the source data a bit.

--- lib/fieldy/writer.rb
+++ lib/fieldy/writer.rb
@@ -10,7 +10,7 @@ module Fieldy
     module InstanceMethods
       def write
         fields = self.class.instance_eval { @fields }
-        data = fields.map        { |x| x.keys.first }
+        data = fields.map        { |x| x[:key] }
                      .select     { |x| x }
                      .reduce({}) { |x, key| x.merge! key => self.send(key) }
         self.class.write data
@@ -20,17 +20,16 @@ module Fieldy
     module WriterMethods
 
       def write(hash)
-        values  = @fields.map { |x| x.keys[0] }
+        values  = @fields.map { |x| x[:key] }
                          .map { |f| hash[f] || '' }
-        pack    = @fields.map { |f| f.values[0] }
-                         .map { |v| "#{v[:type]}#{v[:length]}" }
+        pack    = @fields.map { |v| "#{v[:type]}#{v[:length]}" }
                          .join('')
         values.pack pack
       end
 
       def field(sym, length, type = "A")
         @fields ||= []
-        @fields << { sym => { :length => length, :type => type }}
+        @fields << { key: sym, :length => length, :type => type }
         self.send(:attr_accessor, sym) if sym
       end
 
