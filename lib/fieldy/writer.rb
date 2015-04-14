0c572837ed413170845c39305cb66382a9f3e496
Darren Cauthon <darren@cauthon.com>
2015-04-14 14:36:17 -0500

    Switch to using nil instead of the :null field.

--- lib/fieldy/writer.rb
+++ lib/fieldy/writer.rb
@@ -12,7 +12,8 @@ module Fieldy
         fields = self.class.instance_eval { @fields }
         data = fields.reduce({}) do |t, i|
                  key = i.keys.first
-                 t.merge!(key => self.send(key))
+                 t.merge!(key => self.send(key)) if key
+                 t
                end
         self.class.write data
       end
@@ -24,7 +25,7 @@ module Fieldy
         res = @fields.reduce([]) do |res, f|
                 res + f.flat_map  { |f| f[0] }
                        .flat_map { |k| hash.map  { |x| x[1] }
-                                           .map  { |x| (k == :null ? '' : x) }
+                                           .map  { |x| (k.nil? ? '' : x) }
                                  }
               end
         pack = @fields.map { |f| f.values[0] }
@@ -36,11 +37,11 @@ module Fieldy
       def field(sym, length, type = "A")
         @fields ||= []
         @fields << { sym => { :length => length, :type => type }}
-        self.send(:attr_accessor, sym)
+        self.send(:attr_accessor, sym) if sym
       end
 
       def skip(length)
-        self.field(:null, length, "A" )
+        self.field(nil, length, "A" )
       end
     end
 
