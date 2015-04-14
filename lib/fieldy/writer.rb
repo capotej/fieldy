b92916522983e5b2af3a1a0edca652e402484b7f
Darren Cauthon <darren@cauthon.com>
2015-04-14 14:23:55 -0500

    Rename.

--- lib/fieldy/writer.rb
+++ lib/fieldy/writer.rb
@@ -27,10 +27,10 @@ module Fieldy
             hash.each { |x, y| res << (k == :null ? ' ' : y) }
           end
         end
-        pack_str = @fields.map { |f| f.values[0] }
-                          .map { |v| "#{v[:type]}#{v[:length]}" }
-                          .join('')
-        res.pack(pack_str)
+        pack = @fields.map { |f| f.values[0] }
+                      .map { |v| "#{v[:type]}#{v[:length]}" }
+                      .join('')
+        res.pack pack
       end
 
       def field(sym, length, type = "A")
